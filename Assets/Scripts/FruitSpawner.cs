7f35042df17224f99a2946fa4e234f51762aacfe
Alifa Hafida Anwar <alifahafida@gmail.com>
2022-04-28 00:22:19 +0700

    Spawn fruits

---
+++ Assets/Scripts/FruitSpawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitSpawner : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject[] fruits;
+	private BoxCollider col;
+	float x1, x2;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        col = GetComponent<BoxCollider> ();
+
+	x1 = transform.position.x - col.bounds.size.x/2f;
+	x2 = transform.position.x + col.bounds.size.x/2f;
+		
+    }
+
+    // Update is called once per frame
+    void Start()
+    {
+        StartCoroutine (SpawnFruit(1f));
+    }
+	IEnumerator SpawnFruit(float time) {
+	yield return new WaitForSecondsRealtime (time);
+	
+	Vector3 temp = transform.position;
+	temp.x = Random.Range (x1, x2);
+
+	Instantiate (fruits[Random.Range(0, fruits.Length)], temp, Quaternion.identity);
+
+	StartCoroutine (SpawnFruit(Random.Range(1f, 2f)));
+	}
+}
