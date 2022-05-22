231560800320341c423cb78a9f17f3d9c69853ac
Putri Ainur Fitri <putri.ainur.71@gmail.com>
2022-05-22 20:19:45 +0700

    add scoring (unfinished)

---
+++ Assets/Scripts/PlayerScore.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Collections;
+
+public class PlayerScore : MonoBehaviour {
+    
+    //private TextMeshProUGUI scoreText;
+    private Text scoreText;
+
+    private int score = 0;
+
+    void Awake() {
+        //scoreText = GameObject.Find("ScoreText").GetComponent<TextMeshProUGUI>();
+        scoreText = GameObject.Find("ScoreText").GetComponent<Text>();
+        scoreText.text = "0";
+    }
+
+    void OnTriggerEnter (Collider target) {
+        if(target.tag == "Fruit") {
+            target.gameObject.SetActive(false);
+            score += 1;
+            Debug.Log(score);
+            scoreText.text = score.ToString();
+            Destroy(target.gameObject);
+        }
+    }
+}
