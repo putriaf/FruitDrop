364481de3a1f47fb3a39c1a5de3fa23497a1408f
Alifa Hafida Anwar <alifahafida@gmail.com>
2022-05-22 20:22:35 +0700

    destroy

---
+++ Assets/Scripts/Destroyer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider col)
+{
+
+        Destroy(this.gameObject); // <== Remove colliding object
+        
+}
+}
