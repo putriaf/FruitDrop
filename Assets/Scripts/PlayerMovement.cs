0f542ddf804c80b28f54d1bb3b7dbeaec2a9a849
Alifa Hafida Anwar <alifahafida@gmail.com>
2022-05-30 09:41:56 +0700

    player bound touch screen

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,10 @@ public class PlayerMovement : MonoBehaviour
     private Touch touch;
     private float speedModifier;
 	Rigidbody rb; //Tells script there is a rigidbody, we can use variable rb to reference it in further script
+    float worldWidth         = 14f;  //serialized, example value
+    float ratioScreenToWorld = 2.0f; //serialized, example value
+
+    float screenWidth        = (float) Screen.width;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -22,11 +26,24 @@ public class PlayerMovement : MonoBehaviour
     rb.MovePosition(transform.position + transform.right * v * speed);
 	// float h = Input.GetAxis("Horizontal") * speed;
     // rb.transform.Translate(h,0,0);
-	if(Input.touchCount >0){
+	// if(Input.touchCount >0){
+    //     touch = Input.GetTouch(0);
+    //     if(touch.phase == TouchPhase.Moved){
+    //         //transform.position.x
+    //         rb.MovePosition(transform.position + transform.right * touch.deltaPosition.x * speed);
+    //     }
+    // }
+    
+
+    if (Input.touchCount > 0)
+    {
         touch = Input.GetTouch(0);
-        if(touch.phase == TouchPhase.Moved){
-            //transform.position.x
-            rb.MovePosition(transform.position + transform.right * touch.deltaPosition.x * speed);
+        if (touch.phase == TouchPhase.Moved)
+        {
+            float horizontal = touch.deltaPosition.x;
+            transform.position +=
+                new Vector3(0f, 0f, horizontal * (worldWidth / screenWidth) * ratioScreenToWorld);
+            //clamp, or whatever
         }
     }
     }
